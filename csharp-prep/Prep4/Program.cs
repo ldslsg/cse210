4ab0926950e941490a44f6c97362fe5d89f53948
SeungGyu Lee <lee20027@byui.edu>
2023-09-20 13:58:28 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -30,5 +30,20 @@ class Program
     }
 
     Console.WriteLine($"The sum is: {sum}");
+
+    float average = ((float)sum) / numbers.Count;
+    Console.WriteLine($"The average is: {average}");
+
+    int max = numbers[0];
+
+    foreach (int number in numbers)
+    {
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+
+    System.Console.WriteLine($"The max is: {max}");
 }
 }
