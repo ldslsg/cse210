7a4a306322037132b22ea8881e69726f2f38d9a8
SeungGyu Lee <lee20027@byui.edu>
2023-10-14 15:44:58 -0600

    2023.10.14

---
+++ ClassActivity/fractions/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Fraction f = new Fraction(3);
+
+Console.WriteLine(f.GetTop());
+
+
+f.SetTop(1);
+
+Console.WriteLine(f.GetTop());
+
+Console.WriteLine(f.GetFractionString());
+
+Console.WriteLine(f.GetDecimalValue());
+
