0d3916568a4514c5bd7043b2baa1fb500f5ccbf7
SeungGyu Lee <lee20027@byui.edu>
2023-12-09 15:38:33 -0700

    final

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,23 @@
-using System;
-
-class Program
+﻿class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create some products
+        Product product1 = new Product { Name = "Product 1", ProductId = 1, Price = 10, Quantity = 2 };
+        Product product2 = new Product { Name = "Product 2", ProductId = 2, Price = 20, Quantity = 1 };
+
+        // Create an address
+        Address address = new Address { Street = "123 Main St", City = "Anytown", State = "CA", Country = "USA" };
+
+        // Create a customer
+        Customer customer = new Customer { Name = "John Doe", Address = address };
+
+        // Create an order
+        Order order = new Order { Products = new List<Product> { product1, product2 }, Customer = customer };
+
+        // Display the packing label, shipping label, and total cost
+        Console.WriteLine("Packing Label:\n" + order.GetPackingLabel());
+        Console.WriteLine("Shipping Label:\n" + order.GetShippingLabel());
+        Console.WriteLine("Total Cost: $" + order.GetTotalCost());
     }
 }
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,26 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Create an address
+        Address address = new Address("123 Main St", "Anytown", "Anystate", "12345");
+
+        // Create a lecture
+        Lecture lecture = new Lecture("Intro to C#", "A beginner's guide to C#", "11/27/2023", "10:00 AM", address, "John Doe", 100);
+        Console.WriteLine(lecture.GetFullDetails());
+        Console.WriteLine(lecture.GetStandardDetails());
+        Console.WriteLine(lecture.GetShortDescription());
+
+        // Create a reception
+        Reception reception = new Reception("Company Party", "A fun party for all employees", "12/15/2023", "6:00 PM", address, "rsvp@company.com");
+        Console.WriteLine(reception.GetFullDetails());
+        Console.WriteLine(reception.GetStandardDetails());
+        Console.WriteLine(reception.GetShortDescription());
+
+        // Create an outdoor gathering
+        OutdoorGathering gathering = new OutdoorGathering("Company Picnic", "A picnic for all employees and their families", "06/15/2024", "12:00 PM", address, "Sunny");
+        Console.WriteLine(gathering.GetFullDetails());
+        Console.WriteLine(gathering.GetStandardDetails());
+        Console.WriteLine(gathering.GetShortDescription());
     }
 }
